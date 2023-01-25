f9790d607f8c8d93ecda3144686874bce0300dbe
Anna <84335136+ny0nna@users.noreply.github.com>
2023-01-25 14:54:58 +0300

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=541-19_flwr',
+    'username' => '541-19_flwr',
+    'password' => 'nynna_0N',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
